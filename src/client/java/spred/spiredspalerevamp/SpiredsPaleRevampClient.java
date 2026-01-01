1e6525790cb7f4326fd682621c1f0218a5121e17
Michael <96860840+SpiredFormula@users.noreply.github.com>
2026-01-01 20:54:09 +1100

    initial commit

---
+++ src/client/java/spred/spiredspalerevamp/SpiredsPaleRevampClient.java
@@ -0,0 +1,10 @@
+package spred.spiredspalerevamp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiredsPaleRevampClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/spred/spiredspalerevamp/util/ModTags.java
@@ -0,0 +1,8 @@
+package spred.spiredspalerevamp.util;
+
+import net.minecraft.registry.tag.TagKey;
+
+public class ModTags {
+    public static void initialize(){
+    }
+}
