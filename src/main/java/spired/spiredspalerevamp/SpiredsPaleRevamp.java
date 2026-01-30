9a7b5f6404b516ff4199361a82960448e27ae538
Michael <96860840+SpiredFormula@users.noreply.github.com>
2026-01-30 22:42:30 +1100

    Added pale explorer map trade

--- src/main/java/spired/spiredspalerevamp/SpiredsPaleRevamp.java
+++ src/main/java/spired/spiredspalerevamp/SpiredsPaleRevamp.java
@@ -2,12 +2,31 @@ package spired.spiredspalerevamp;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.block.Block;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.map.MapDecorationTypes;
+import net.minecraft.item.map.MapState;
+import net.minecraft.registry.tag.StructureTags;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spired.spiredspalerevamp.block.ModBlocks;
 import spired.spiredspalerevamp.entity.ModBlockEntities;
 import spired.spiredspalerevamp.item.ModItems;
 import spired.spiredspalerevamp.util.ModTags;
+
+import java.util.Optional;
 
 public class SpiredsPaleRevamp implements ModInitializer {
 	public static final String MOD_ID = "spireds-pale-revamp";
@@ -26,7 +45,30 @@ public class SpiredsPaleRevamp implements ModInitializer {
 		ModTags.initialize();
 		ModBlocks.initialize();
 		ModItems.initialize();
+
+
+		// TODO moved to separate class
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CARTOGRAPHER, 1, factories -> {
+			factories.add((world, entity, random) -> {
+				BlockPos structurePos = world.locateStructure(ModTags.ON_PALE_EXPLORER_MAPS, entity.getBlockPos(),  100, true);
+				int price = 1;
+				int maxUses = 12;
+				int experience = 30;
+				if(structurePos != null){
+					ItemStack map = FilledMapItem.createMap(world, structurePos.getX(), structurePos.getZ(), (byte)2, true, true);
+					FilledMapItem.fillExplorationMap(world, map);
+					MapState.addDecorationsNbt(map, structurePos, "+", MapDecorationTypes.TRIAL_CHAMBERS);
+					map.set(DataComponentTypes.ITEM_NAME, Text.translatable("filled_map.custom"));
+					return new TradeOffer(new TradedItem(Items.EMERALD, price), Optional.of(new TradedItem(Items.COMPASS)), map, maxUses, experience, 0.2F);
+				}
+				else{
+					return null;
+				}
+			});
+
+		});
 		LOGGER.info("Hello Fabric world!");
+
 
 	}
 }
